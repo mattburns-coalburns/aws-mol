dee79c061c6b8b9931c192f5f33b2145a00c4fbf
mattburns-coalburns <matt.burns@coalfire.com>
2022-03-31 16:45:22 -0400

    add chapter 2 terraform code

--- chapters/ch2/README.md
+++ chapters/ch2/README.md
@@ -1,5 +1,24 @@
 # The 10-minute EC2 Web Server
 
+
+## Instructions
+- Open a Terminal
+- `cd chapters/ch2/`
+- `terraform init`
+- `terraform apply auto-approve`
+- An SSH private key will be created in the current directory
+- Look at the outputs and copy/paste the string to SSH to the EC2 instance.
+- Also, go to the link in the outputs.
+
+# Resources in this lab
+- VPC
+- Subnet
+- Internet Gateway
+- Route Table
+- Security Groups
+- EC2 Instance
+- RSA 4096 Keys
+
 ## Goals
 - Launch an EC2 Ubuntu Linux server instance
 - Use SSH to connect with your instance
--- chapters/ch2/main.tf
+++ chapters/ch2/main.tf
@@ -1,20 +1,75 @@
+###############################
+#        Provider Block       #
+###############################
+
 # Provider Block
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.8.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.2.2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
+  region = "us-east-1"
 }
+
+provider "local" {}
+
+provider "tls" {}
+
+###############################
+#          Variables          #
+###############################
 
 variable "prefix" {
   default = "aws-mol"
 }
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "az" {
+  default = "us-east-1a"
+}
+
+###############################
+#        Local SSH Key        #
+###############################
+
+# Creates RSA 4096-encrypted key pair
+resource "tls_private_key" "web" {
+  algorithm   = "RSA"
+  ecdsa_curve = "4096"
+}
+
+# Saves the private .pem file locally
+resource "local_file" "web" {
+  filename        = "web_server.pem"
+  file_permission = 0400
+  content         = tls_private_key.web.private_key_pem
+}
+
+# Provides Public key to the Web Server EC2 instance
+resource "aws_key_pair" "web" {
+  key_name   = "web_server"
+  public_key = tls_private_key.web.public_key_openssh
+}
+
+###############################
+#             VPC             #
+###############################
 
 # VPC
 resource "aws_vpc" "vpc" {
@@ -29,18 +84,10 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = var.az
 
   tags = {
     Name = "${var.prefix}-public-subnet"
-  }
-}
-
-# EIP for Internet Gateway
-resource "aws_eip" "igw_eip" {
-  vpc = true
-  tags = {
-    Name = "${var.prefix}_igw_eip"
   }
 }
 
@@ -94,6 +141,35 @@ resource "aws_security_group" "pub_http_sg" {
   }
 }
 
+# Provides Security Group for Public SSH Access
+resource "aws_security_group" "pub_ssh_sg" {
+  name        = "ssh_access_public"
+  description = "Allows SSH access from the public internet"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    description = "SSH port 22 from public internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description      = "SSH port 22 to public internet"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "public_ssh_access_sg"
+  }
+}
+
+###############################
+#             EC2             #
+###############################
+
 # AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -113,21 +189,47 @@ data "aws_ami" "ubuntu" {
 
 # EC2 Instance
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  subnet_id = aws_subnet.public.id
-  instance_type = "t3.micro"
+  ami                         = data.aws_ami.ubuntu.id
+  subnet_id                   = aws_subnet.public.id
+  instance_type               = "t3.micro"
   associate_public_ip_address = true
-  availability_zone = "us-east-1a"
-  vpc_security_group_ids      = [aws_security_group.pub_http_sg.id]
-  
+  key_name = aws_key_pair.web.key_name
+  vpc_security_group_ids = [
+    aws_security_group.pub_http_sg.id,
+    aws_security_group.pub_ssh_sg.id
+  ]
+
   ebs_block_device {
-    delete_on_termination = "true"
-    device_name = "/dev/sda1"
-    volume_size = "8"
+    delete_on_termination = true
+    device_name           = "/dev/sda1"
+    volume_size           = "8"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install apache2 -y
+    apt install mysql-server -y
+    apt install php libapache2-mod-php php-mysql -y
+    apt install php-curl php-json php-cgi -y
+    systemctl enable apache2
+    systemctl start apache2
+    echo '<h1>Welcome!</h1>' > /var/www/html/index.html
+    echo "We hope you <i>really</i> enjoy your stay here." >> /var/www/html/index.html
+    EOF
 
   tags = {
     Name = "${var.prefix}-web"
   }
 }
 
+###############################
+#           Outputs           #
+###############################
+output "web_ssh" {
+  value = "Type this in on a terminal to SSH connection to web server: ssh -i ${local_file.web.filename} ubuntu@${aws_instance.web.public_ip}"
+}
+
+output "web_url" {
+  value = "http://${aws_instance.web.public_ip}/"
+}
